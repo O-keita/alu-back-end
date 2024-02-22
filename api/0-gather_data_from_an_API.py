d8824e979168c885aab075c2f6a4bef0c6dfad21
O-keita <o.keita@alustudent.com>
2024-02-22 21:33:47 +0200

    one done

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Fisrt question with APIs"""
+
+import requests
+import sys
+
+
+def main():
+    """main function"""
+    user_id = int(sys.argv[1])
+
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+
+    todo_res = requests.get(todo_url)
+
+    number_of_task = 0
+
+    completed_tasks =[]
+
+    for todo in todo_res.json():
+        if todo['userId'] == user_id:
+            number_of_task += 1
+
+            if todo['completed']:
+                completed_tasks.append(todo['title'])
+
+    user_name = requests.get(user_url).json()['name']
+
+    output = "Employee {} is done with tasks({}/{}):".format(user_name, len(completed_tasks),
+                                                              number_of_task)
+
+    print(output)
+
+    for task_title in completed_tasks:
+        print("\t {}".format(task_title))
+
+
+if __name__ == "__main__":
+    main()
+
