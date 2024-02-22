d2c42f13febd2d92ca784a596d6d8adb929434fa
O-keita <o.keita@alustudent.com>
2024-02-22 23:35:55 +0200

    done with three

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Export to JSON using dump"""
+"""Script that gets user data (Todo list) from API
+and then export the result to csv file. """
 
 import json
 import requests
@@ -8,29 +9,27 @@ import sys
 
 def main():
     """main function"""
-
     user_id = int(sys.argv[1])
     todo_url = 'https://jsonplaceholder.typicode.com/todos'
     user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
 
-    user_info = []
-    user_file = {user_id: user_info}
-
     response = requests.get(todo_url)
     user_name = requests.get(user_url).json().get('username')
+    user_data = []
+    output = {user_id: user_data}
 
     for todo in response.json():
         if todo.get('userId') == user_id:
-            user_info.append({
-                "task": todo.get('title'),
-                "completed": todo.get('completed'),
-                "name": user_name
-            })
-
+            user_data.append(
+                {
+                    "task": todo.get('title'),
+                    "completed": todo.get('completed'),
+                    "username": user_name,
+                })
+    print(output)
     file_name = "{}.json".format(user_id)
-
-    with open(file_name, 'w', newline='') as json_file:
-        json.dump(user_file, json_file)
+    with open(file_name, 'w') as file:
+        json.dump(output, file)
 
 
 if __name__ == '__main__':
